6ce79ec0610a1b1fa4542f68bb49f0b3ef6506b2
DekuDeshayan <buraimoRalph129@gmail.com>
2022-03-27 15:07:57 +0200

    Project created

---
+++ src/main/java/com/ujc/clouddishes/CloudDishesApplication.java
@@ -0,0 +1,13 @@
+package com.ujc.clouddishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDishesApplication.class, args);
+	}
+
+}
